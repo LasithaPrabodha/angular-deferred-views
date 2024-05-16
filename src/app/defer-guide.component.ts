37d1922eda5255650c00c28f44789140c09bf9f6
Lasitha Weligampola <lasiprabo@gmail.com>
2024-05-16 12:09:45 -0400

    playwright fix

--- src/app/defer-guide.component.ts
+++ src/app/defer-guide.component.ts
@@ -49,7 +49,41 @@ import { CommonModule } from '@angular/common';
           created, which is reflected while building Angular applications:
         </li>
       </ul>
-      <pre></pre>
+      <pre>
+      Initial chunk files   | Names                            |  Raw size | Estimated transfer size
+      chunk-B4PT2UKD.js     | -                                | 135.04 kB |                40.38 kB
+      chunk-H4XLK2F4.js     | -                                |  86.48 kB |                21.65 kB
+      polyfills-S3BTP7ME.js | polyfills                        |  33.24 kB |                10.76 kB
+      main-GNS2ZULW.js      | main                             |   9.68 kB |                 2.60 kB
+      chunk-JPMP5HGK.js     | -                                |   8.14 kB |                 2.15 kB
+      chunk-IXQEDF2Q.js     | -                                |   2.32 kB |               593 bytes
+      styles-5CJZHWYF.css   | styles                           |   1.79 kB |               512 bytes
+
+                            | Initial total                    | 276.69 kB |                78.63 kB
+
+      Lazy chunk files      | Names                            |  Raw size | Estimated transfer size
+      chunk-ZXLJYGDC.js     | -                                |  19.61 kB |                 4.70 kB
+      chunk-C4VTOYQ7.js     | defer-loading-vs-placeholder     |   3.08 kB |                 1.11 kB
+      chunk-7WRCO6C5.js     | defer-accordion                  |   2.92 kB |                 1.09 kB
+      chunk-J5YM6I44.js     | defer-on-viewport                |   2.92 kB |                 1.22 kB
+      chunk-4H7FKA2E.js     | defer-on-hover                   |   2.92 kB |                 1.04 kB
+      chunk-75CQGKNI.js     | defer-npm-package                |   2.83 kB |                 1.20 kB
+      chunk-H5NUBHFD.js     | snippet-dialog-content-component |   2.78 kB |                 1.10 kB
+      chunk-EUVWVKIA.js     | defer-when-condition             |   2.69 kB |                 1.11 kB
+      chunk-EK5XJ65T.js     | use-case-container-component     |   2.63 kB |                 1.09 kB
+      chunk-ABAAB7NP.js     | defer-on-idle                    |   2.31 kB |              1020 bytes
+      chunk-5DWJK6PK.js     | defer-on-interaction             |   2.12 kB |               933 bytes
+      chunk-AKNTASCX.js     | defer-prefetch                   |   2.09 kB |               928 bytes
+      chunk-ZLZRARPG.js     | defer-on-immediate               |   1.95 kB |               884 bytes
+      chunk-LTI7K6QV.js     | defer-error                      |   1.83 kB |               887 bytes
+      chunk-2G44LGPA.js     | defer-on-timer                   |   1.81 kB |               36m824 bytes
+      ...and 29 more lazy chunks files. Use "--verbose" to show all the files.
+
+      Output location: /Users/lasithaw/Projects/angular-17/angular-deferred-views/docs
+
+      Application bundle generation complete. [2.894 seconds]
+
+      </pre>
       Recommended reads:
       <ul>
         <li>
